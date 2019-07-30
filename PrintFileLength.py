3a14e24fa7927b3435a62b76a7808f1d31575f65
DBerrones <53327546+danielberrones@users.noreply.github.com>
2019-07-29 20:57:50 -0500

    Add files via upload

---
+++ PrintFileLength.py
@@ -0,0 +1,20 @@
+## Reads the contents of a file in Terminal
+## Created by Daniel Berrones
+
+from sys import argv
+script, filename = argv
+
+def main():
+	f = open(filename,"r")
+	data = f.read()
+	length = len(data)
+	print("\n\n************************************************")
+	print(f"\tCONTENTS OF FILE: {filename}")
+	print("************************************************")
+	print("\n\n",data)
+	print("\n************************************************")
+	print("************* FILE LENGTH: {}".format(length)+"   *************")
+	print("************************************************")
+
+if __name__ == "__main__":
+	main()
