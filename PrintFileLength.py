c9c1f797fcad0b6a8d1b9b1d2812fc4b030f2332
Daniel Berrones <daniel.a.berrones@gmail.com>
2020-05-19 20:04:27 -0500

    Updated Code

--- PrintFileLength.py
+++ PrintFileLength.py
@@ -9,13 +9,11 @@ def main():
 	f = open(filename,"r")
 	data = f.read()
 	length = len(data)
-	print("\n\n************************************************")
 	print(f"\tCONTENTS OF FILE: {filename}")
 	print("************************************************")
 	print("\n\n",data)
-	print("\n************************************************")
-	print("************* FILE LENGTH: {}".format(length), "*************")
-	print("************************************************")
+	print("\n************* FILE LENGTH: {}".format(length), "*************")
+	print("************************************************\n")
 
 
 if __name__ == "__main__":
